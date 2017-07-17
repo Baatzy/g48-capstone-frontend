86b40a30942a23e22844c6d2cf61939be6563a80
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-07-17 12:47:57 -0700

    Added components for dashboard

--- src/App.js
+++ src/App.js
@@ -2,17 +2,90 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class NavContainer extends Component {
+  render () {
+    return (
+      <div className="App-header">
+        <h2>✨ The Boardroom ✨</h2>
+        <p>This is the NavContainer Component</p>
+      </div>
+    )
+  }
+}
+
+class TrainNowButton extends Component {
+  render () {
+    return (
+      <div className="Dash-button">
+        <button>Train Now</button>
+      </div>
+    )
+  }
+}
+
+class ViewCalendarButton extends Component {
+  render () {
+    return (
+      <div className="Dash-button">
+        <button>View Calendar</button>
+      </div>
+    )
+  }
+}
+
+class ViewProtocolsButton extends Component {
+  render () {
+    return (
+      <div className="Dash-button">
+        <button>See Protocols</button>
+      </div>
+    )
+  }
+}
+
+class Snapshot extends Component {
+  render () {
+    return (
+      <div>
+        <p>Upcomming workouts and dashboard stats go here</p>
+        <p>Upcomming workouts and dashboard stats go here</p>
+        <p>Upcomming workouts and dashboard stats go here</p>
+        <p>Upcomming workouts and dashboard stats go here</p>
+        <p>Upcomming workouts and dashboard stats go here</p>
+      </div>
+    )
+  }
+}
+
+class BodyContainer extends Component {
+  render () {
+    return (
+      <div>
+        <TrainNowButton />
+        <ViewCalendarButton />
+        <ViewProtocolsButton />
+        <Snapshot />
+      </div>
+    )
+  }
+}
+
+class Dashboard extends Component {
+  render () {
+    return (
+      <div>
+        <NavContainer />
+        <BodyContainer />
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dashboard />
       </div>
     );
   }
